42e6684a4cefb93bd3787e26c91e88173bd5bae0
Oleksandr Bukovskyi <oleksandrbu@tahometer.com>
2020-08-05 12:13:14 +0300

    Ready

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Dictionary
 {
@@ -6,7 +7,30 @@ namespace Dictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String str = "Split the original string into the string after the delimiter and the remainder of the original string after the delimiter";
+            String[] wordList = str.Split(' ', StringSplitOptions.None);
+            Dictionary<string, uint> mainDictionary = GetWordCount(wordList);
+
+            Dictionary<string, uint>.KeyCollection strList = mainDictionary.Keys;
+            Console.WriteLine("{0} {1}", "Key", "Values");
+            foreach(string strEl in strList)
+            {
+                Console.WriteLine("{0} {1}", strEl, mainDictionary[strEl]);
+            }
+        }
+
+        static public Dictionary<string, uint> GetWordCount(string[] wordList){
+            Dictionary<string, uint> curDictionary = new Dictionary<string, uint>();
+            foreach(string str in wordList)
+            {
+                if (curDictionary.ContainsKey(str)){
+                    curDictionary[str] ++;
+                } else {
+                    curDictionary.Add(str, 1);
+                }
+            }
+
+            return curDictionary;
         }
     }
 }
